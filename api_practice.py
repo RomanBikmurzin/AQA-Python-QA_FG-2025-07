15180afa3cf074bcf842331c515783e99a2b53d1
me <romanbikmurzin.smr@gmail.com>
2025-07-18 08:14:59 +0400

    api practice: added GitHub Action Secrets to provide successfully authorization

--- api_practice.py
+++ api_practice.py
@@ -12,9 +12,13 @@ load_dotenv()
 # ---------- Протестировать эндпоинт /api/login ----------
 @pytest.fixture
 def api_client():
+    try:
+        api_key = os.environ["X_API_KEY"]      # бросит KeyError, если нет
+    except KeyError as e:
+        pytest.skip("X_API_KEY not set in env") # тест отметится SKIPPED
     return {
         "base_url": "https://reqres.in",
-        "headers": {"x-api-key": os.getenv("X_API_KEY")},
+        "headers": {"x-api-key": api_key},
     }
 
 
